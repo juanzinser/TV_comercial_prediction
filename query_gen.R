dd21551f3cad2048cdd146ce80ee399a709415e1
ebalp <ebalp@yahoo.com>
2014-08-18 18:46:45 -0500

    updated queries

--- query_gen.R
+++ query_gen.R
@@ -9,8 +9,8 @@ final <- as.Date("2014-08-10")
 seq <- seq(from = inicio, to=final,by = 1)
 #paste(year(seq[20]),sprintf("%02d",month(seq[20])),sprintf("%02d",day(seq[20])),sep="")
 
-query <- "SELECT date, hits.hour,hits.minute, visits, newvisits, pageviews, hits, transactions from"
-p1 <- "(SELECT date, hits.hour,hits.minute, sum(totals.visits) visits, sum(totals.newVisits) newvisits, sum(totals.pageviews) pageviews,sum(totals.hits) hits, sum(totals.transactions) transactions FROM [ace-amplifier-455:58090804.ga_sessions_"
+query <- "SELECT date, hits.hour,hits.minute, visits, newvisits, transactions from"
+p1 <- "(SELECT date, hits.hour,hits.minute, sum(totals.visits) visits, sum(totals.newVisits) newvisits, sum(totals.transactions) transactions FROM [ace-amplifier-455:58090804.ga_sessions_"
 p2 <- "] where trafficSource.source not like 'Postal%' and hits.time = 0 group by date, hits.hour, hits.minute)"
 
 for(i in 1:length(seq)){
