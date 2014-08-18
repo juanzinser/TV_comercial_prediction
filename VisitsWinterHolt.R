d3037a3bd5782ceba443e881baf5fb6e39e1bcf0
ebalp <ebalp@yahoo.com>
2014-08-18 18:49:11 -0500

    preprocesamiento

---
+++ VisitsWinterHolt.R
@@ -0,0 +1,26 @@
+# Look at https://www.otexts.org/fpp/7/5 for Holt-Winters Method
+
+# First install.packages("fpp")
+
+library(fpp)
+
+visitsraw <- read.csv("WebTrekk Daily Visits.csv")
+visitsraw$Days <- as.POSIXct(visitsraw$Days,format="%d/%m/%Y" )
+start <- as.POSIXct("2014-05-26", format="%Y-%m-%d")
+end <- as.POSIXct("2014-08-10", format="%Y-%m-%d")
+v <- visitsraw[which(visitsraw$Days>=start & visitsraw$Days<=end),]
+
+vts <- ts(v$Visits,frequency=7,start=1)
+hwfit <- hw(vts,seasonal="multiplicative", initial="optimal")
+
+plot(hwfit,ylab="Visits",
+     plot.conf=TRUE, type="o", fcol="white", xlab="Week", main="Visits prediction")
+lines(fitted(hwfit), col="blue", lty=2)
+lines(hwfit$mean, type="o", col="blue")
+legend("topleft",lty=1, pch=1, col=c("black","blue"),   c("Actual","Model"))
+
+vpos <- visitsraw[which(visitsraw$Days>end),]$Visits
+forpos <- hwfit$fitted[1:length(vpos)]
+
+# The error for the two points we have data for (Aug 11th and 12th) is 3.8% and 2% 
+error <- (forpos-vpos)/vpos
---
+++ preprocesamiento.R
@@ -0,0 +1,53 @@
+
+## ------ PREPROCESSING -----------
+preprocess <- FALSE
+if(preprocess){
+  v <- read.csv("20140714_0810.csv")
+  tam <- length(v$visits)
+  ndays <- tam/1440 
+  
+## Parameters 
+  
+  hlfwin <- 30
+  
+## Timestmp
+  v$tmstmp <- as.POSIXlt(paste(v$date,v$hits_hour,v$hits_minute), format = "%Y%m%d %H %M")
+
+
+## Missing rows
+## existingDF <- rbind(existingDF[1:r,],newrow,existingDF[-(1:r),])
+
+tmseq <- seq(v$tmstmp[1],tail(v$tmstmp,n=1), by="min")
+
+repeat {
+  miss <- which.min(tmseq[1:length(v$tmstmp)] == v$tmstmp)
+  if(miss==1) break
+  newrow <- data.frame(date = v$date[miss-1],hits_hour = hour(tmseq[miss]), 
+                       hits_minute = minute(tmseq[miss]), visits = 0, newvisits = 0, 
+                       transactions = 0, tmstmp = tmseq[miss])
+  v <- rbind(v[1:miss-1,],newrow,v[miss:length(v$date),])
+}
+tam <- length(v$visits)
+
+## Date
+
+  v$date <- as.POSIXct(as.character(v$date), format = "%Y%m%d")
+  v$wday <- wday(v$date)
+  
+## Smoothing
+  v$v <- numeric(tam)
+  v$nv <- numeric(tam)
+  for (t in 1:tam){
+    v$v[t]<-mean(v$visits[max(1,t-hlfwin):min(tam,t+hlfwin-1)])
+    v$nv[t]<-mean(v$newvisits[max(1,t-hlfwin):min(tam,t+hlfwin-1)])
+  }
+}
+
+par(mfrow=c(2,1))
+
+xmat <- matrix(v$v,nrow=1440*7)
+xmean <- rowMeans(xmat)
+matplot(cbind(matrix(v$visits,nrow=1440*7),xmean), type = c('l','l','l','l',"b"),pch=1,col = c(2:5,1), ylab = "Actual Visits", main="Comparing with 4-week Avg (hlfwin=30)")
+legend("topleft", legend = 1:4, col=2:5, pch=1)
+matplot(cbind(xmat,xmean), type = c('l','l','l','l',"b"),pch=1,col = c(2:5,1), ylab = "Smoothed Visits")
+legend("topleft", legend = 1:4, col=2:5, pch=1)
